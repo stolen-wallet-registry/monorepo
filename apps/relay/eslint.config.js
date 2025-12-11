cfc2ca7777b4afc5c89c2e5574647b05ce979b35
brossetti1 <brossetti1@gmail.com>
2025-12-11 14:34:23 +0700

    eslint fixes for relayer

---
+++ apps/relay/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    ignores: ['.data/', 'node_modules/'],
+  },
+];
